97c3c8bfafb7a8a9bc9cbb5b784680dcc73f1193
Sing-Kai Chiu <singkai.chiu@gmail.com>
2023-03-28 10:32:27 +0100

    update client

--- kana-client/src/App.js
+++ kana-client/src/App.js
@@ -2,33 +2,26 @@ import './App.css';
 import Home from "./pages/Home";
 import {
   BrowserRouter as Router,
-  Switch,
+  Routes,
   Route,
-  Link
 } from "react-router-dom";
 import QuizContent from "./components/Quiz";
 import Settings from "./pages/Settings";
 import {createGlobalStyle} from 'styled-components'
 
-const GlobalStyle = createGlobalStyle`
-`;
+const GlobalStyle = createGlobalStyle``;
 
 function App() {
 
   return (
     <Router>
       <div className="App">
-        <Switch>
-            <Route path="/quiz">
-              <QuizContent />
-            </Route>
-            <Route path="/settings">
-              <Settings />
-            </Route>            
-            <Route exact path="/">
-              <Home />
-            </Route>
-        </Switch>
+        <Routes>
+          <Route path="/quiz" element={<QuizContent />}/>
+          <Route path="/settings" element={ <Settings />}/>        
+          <Route exact path="/" element={<Home />}>
+          </Route>
+        </Routes>
       </div>
     </Router>
   );
